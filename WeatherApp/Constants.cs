0341e9e2b16f1cdef6a61d6c1f81854c2fe61334
Martin Jørgensen <satoz.game@gmail.com>
2021-08-18 11:02:09 +0200

    App upload

---
+++ WeatherApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "399825c696bef0bdc535a044c7efd23b";
+    }
+}
---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _restService = new RestService();
+
+
+        }
+
+    
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+
+            }
+
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e) 
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                
+                if (location == null)
+                {
+                    location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30)
+
+                    });
+                }
+                if (location == null)
+                    LabelLocation.Text = "No GPS";
+                else
+                    LabelLocation.Text = $"{location.Latitude} {location.Longitude}";
+
+                
+
+            }
+            catch (Exception)
+            { 
+            }
+        }
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={_cityEntry.Text}";
+        //  requestUri += $"&long={_cityEntry.Text}";
+            requestUri += "&units=metric"; // or units=metric
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+    }
+}
+
+
